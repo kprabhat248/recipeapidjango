a61e15c8f8964ce3d0061c59fa5788b15d09dc09
Prabhat Kumar <prabhatkumar@Prabhats-MacBook-Air.local>
2024-11-03 21:15:50 +0530

    configured admin and usermodel

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,41 @@
-from django.db import models # noqa
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    "Manager for user"
+
+    def create_user(self, email, password=None, **extra_fields):
+        "Create, save and return a new user"
+        if not email:
+            raise ValueError('User must have a valid email address.')
+        email = self.normalize_email(email).lower()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        "Create and return a new superuser"
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True or extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_staff=True and is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    "User in the system"
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']  # Add any other required fields for superuser creation
